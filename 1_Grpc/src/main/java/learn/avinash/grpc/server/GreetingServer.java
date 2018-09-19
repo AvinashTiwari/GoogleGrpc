bfd3a470cf447de07a4dcbb140d0cc961465545a
Avinash Tiwari <qwe123kids@gmail.com>
2018-09-18 19:07:33 -0600

    no message

---
+++ 1_Grpc/src/main/java/learn/avinash/grpc/server/GreetingServer.java
@@ -0,0 +1,31 @@
+package learn.avinash.grpc.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello Grpc");
+
+        Server server = ServerBuilder.forPort(50051)
+                .build();
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received shutting down request");
+
+            server.shutdown();
+
+            System.out.println("Serve Shutdown");
+
+
+
+        }));
+
+        server.awaitTermination();
+
+    }
+}
