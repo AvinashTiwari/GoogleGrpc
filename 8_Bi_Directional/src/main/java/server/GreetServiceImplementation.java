686b8bd534fde21d202c9e08d86c5e7b354e34ed
Avinash Tiwari <qwe123kids@gmail.com>
2018-10-08 18:45:02 -0600

    Bi directional

---
+++ 8_Bi_Directional/src/main/java/server/GreetServiceImplementation.java
@@ -0,0 +1,71 @@
+package server;
+
+import com.proto.greet.*;
+import io.grpc.stub.StreamObserver;
+
+public class GreetServiceImplementation extends GreetServiceGrpc.GreetServiceImplBase{
+
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+        Greeting greeting = request.getGreeting();
+        String firstName = greeting.getFirstName();
+
+        String result = " Hello " + firstName;
+
+        GreetResponse response = GreetResponse.newBuilder()
+                 .setResult(result)
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    //    super.greet(request, responseObserver);
+    }
+
+    @Override
+    public void greetManyTimes(GreetManyTimesRequest request, StreamObserver<GreetManyTimesResponse> responseObserver) {
+        String firstName = request.getGreeting().getFirstName();
+
+        for (int i =0; i <10 ; i++){
+            String result = "Hello " + firstName + " responseNumber " + i;
+
+            GreetManyTimesResponse response = GreetManyTimesResponse.newBuilder()
+                    .setResult(result)
+                    .build();
+
+            responseObserver.onNext(response);
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<GreetEveryoneRequest> greetEveryone(StreamObserver<GreetEveryoneResponse> responseObserver) {
+        StreamObserver<GreetEveryoneRequest> requestObserver = new StreamObserver<GreetEveryoneRequest>() {
+            @Override
+            public void onNext(GreetEveryoneRequest value) {
+                String result = "Hello " + value.getGreeting().getFirstName();
+                GreetEveryoneResponse greetEveryoneResponse = GreetEveryoneResponse.newBuilder()
+                        .setResult(result)
+                        .build();
+
+                responseObserver.onNext(greetEveryoneResponse);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // do nothing
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
+
+        return requestObserver;
+    }
+}
